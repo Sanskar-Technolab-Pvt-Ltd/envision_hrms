751039ebb09a747448b5a267732a87c970ad12ce
poojavadher <pooja@sanskartechnolab.com>
2025-04-03 10:56:12 +0530

    Test Commit

--- envision_hrms/envision_hrms/custom_py/salary_ot_calculation.py
+++ envision_hrms/envision_hrms/custom_py/salary_ot_calculation.py
@@ -16,6 +16,8 @@ def get_total_ot_hours(employee, from_date, to_date):
     total_ot_hours = sum(float(attendance.get('ot_hours', 0)) for attendance in attendance_records)
     total_ph = sum(float(attendance.get('public_holiday', 0)) for attendance in attendance_records)
 
+    # hard reset changes
+
     return {"total_ot_hours": total_ot_hours, "total_ph": total_ph}
 
 
