555c98bf9b3da0048259f75cdae1acb239f4f558
poojavadher <pooja@sanskartechnolab.com>
2025-06-24 12:02:01 +0530

    New Job Offer print format,Add purpose field to attendance request,Exit interview Questionnaire doctyep

---
+++ envision_hrms/envision_hrms/doctype/exit_interview_questionnaire/exit_interview_questionnaire.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Pooja Vadher and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Exit Interview Questionnaire", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ envision_hrms/envision_hrms/doctype/exit_interview_questionnaire/test_exit_interview_questionnaire.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Pooja Vadher and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExitInterviewQuestionnaire(FrappeTestCase):
+	pass
