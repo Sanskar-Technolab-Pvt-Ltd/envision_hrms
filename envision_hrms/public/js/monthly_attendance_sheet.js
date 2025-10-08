0d05c709882de28431843af4265c58613bf5c51c
Priyanka <priyanka@sanskartechnolab.com>
2025-10-08 14:27:21 +0530

    Add role in Payroll Entry Trends Report

--- envision_hrms/public/js/monthly_attendance_sheet.js
+++ envision_hrms/public/js/monthly_attendance_sheet.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
-console.log("Hello")
+// console.log("Hello")
 frappe.query_reports["Monthly Attendance Sheet"] = {
 	filters: [
 		{
